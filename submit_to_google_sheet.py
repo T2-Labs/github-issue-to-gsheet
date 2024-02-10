9c12c66b92149edbf56334a0c5aaf521ec2bf159
twhetzel <plwhetzel@gmail.com>
2024-02-10 00:55:05 -0800

    try to add to gsheet

---
+++ submit_to_google_sheet.py
@@ -0,0 +1,22 @@
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Set up the Google Sheets API credentials
+scope = ['https://www.googleapis.com/auth/spreadsheets']
+creds_json = os.getenv('GOOGLE_SHEETS_CREDENTIALS')
+creds_dict = eval(creds_json)  # Convert JSON string to dictionary
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
+# Authenticate with the Google Sheets API
+client = gspread.authorize(creds)
+
+# Open the Google Sheet by its ID
+sheet_id = 'YOUR_GOOGLE_SHEET_ID'
+sheet = client.open_by_key(sheet_id).sheet1
+
+# Get the issue body from environment variable
+issue_body = os.getenv('ISSUE_BODY')
+
+# Append the issue body to the Google Sheet
+sheet.append_row([issue_body])
