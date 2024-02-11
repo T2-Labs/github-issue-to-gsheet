aee4151f38504b43be8a6905ccb2ddf6cfb50ccb
Trish Whetzel <plwhetzel@gmail.com>
2024-02-10 17:00:33 -0800

    Issue submit structured data (#37)
    * remove older issue template
    
    * update template
    
    * test structured

--- submit_to_google_sheet.py
+++ submit_to_google_sheet.py
@@ -18,8 +18,20 @@ client = gspread.authorize(creds)
 sheet_id = '1s-GTbL1GmHqeLm5gnWKUJEz5wmNSLzELB0fcZAS3RZo'
 sheet = client.open_by_key(sheet_id).sheet1
 
-# Get the issue body from environment variable
-issue_body = os.getenv('ISSUE_BODY')
+# # Get the issue body from environment variable
+# issue_body = os.getenv('ISSUE_BODY')
 
-# Append the issue body to the Google Sheet
-sheet.append_row([issue_body])
+# # Append the issue body to the Google Sheet
+# sheet.append_row([issue_body])
+
+
+# TODO: Parse values from ISSUE_BODY to use for column values 
+
+
+# Define row data for different columns
+column1_data = 'Value1'
+column2_data = 'Value2'
+column3_data = 'Value3'
+
+# Append row data into different columns
+row_data = [column1_data, column2_data, column3_data]
